31f059f786d7abad80759f7de5e713dd22f9b7ec
Thaysa Lacerda <t_thaysa.lacerda@hapvida.com.br>
2024-09-27 06:35:27 -0400

    feat: ajustes visuais dashboard

--- obj/Debug/net8.0/FinanceiroWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/FinanceiroWeb.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceiroWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138636334aba8859afa6a5ceb8a875eb451d5f04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eccd9d901fe81219bca1856c04b4a4155db3eb4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceiroWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceiroWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
